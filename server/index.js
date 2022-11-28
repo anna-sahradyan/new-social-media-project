632e5b6930487f531844709d50f5692a623c7b9c
anna-sahradyan <a.sahradyan72@gmail.com>
2022-11-29 00:40:37 +0400

    add router and models directory

--- server/index.js
+++ server/index.js
@@ -1,4 +1,31 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const authRoute = require("./routes/auth");
+
+
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_KEY,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+        console.log("Connected to MongoDB");
+    }
+);
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/auth", authRoute);
+
+
+
+app.listen(4000, () => {
+    console.log("Backend server is running!");
+});
