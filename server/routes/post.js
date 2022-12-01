75b9d3baeec94b1b41ab42a1561e8cc4eaf64693
anna-sahradyan <a.sahradyan72@gmail.com>
2022-12-01 18:11:02 +0400

    add delete post router

--- server/routes/post.js
+++ server/routes/post.js
@@ -16,16 +16,31 @@ router.put("/:id", async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
         if (post.userId === req.body.userId) {
-            await post.updateOne({$set:req.body});
+            await post.updateOne({$set: req.body});
             res.status(200).json("the post has been updated ")
         } else {
-            res.status(403).json("You can update only your post")
+            res.status(403).json("You can update only your post");
         }
     } catch (err) {
         res.status(500).json(err)
     }
 })
 //! DELETE A POST
+router.delete("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.userId === req.body.userId) {
+            await post.deleteOne();
+            res.status(200).json("the post has deleted");
+
+        } else {
+            res.status(403).json("you can delete only your post");
+        }
+
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 //!LIKE A POST
 //!GET A POST
 //!GET ALL POST
